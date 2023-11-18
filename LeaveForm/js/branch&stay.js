71df14b8b26b7ad012a66f3ed46b7c4cb5661e11
sevengreenstudioltd <150896697+sevengreenstudioltd@users.noreply.github.com>
2023-11-18 23:38:41 +0530

    Add files via upload

---
+++ LeaveForm/js/branch&stay.js
@@ -0,0 +1,23 @@
+const branch = document.querySelector(".branch");
+const staying = document.querySelector(".staying");
+
+const aids = document.querySelector(".aids");
+const cse = document.querySelector(".cse");
+const cseit = document.querySelector(".cseit");
+const me = document.querySelector(".me");
+const eee = document.querySelector(".eee");
+const ece = document.querySelector(".ece");
+const civil = document.querySelector(".civil");
+const mde = document.querySelector(".mde");
+const robotics = document.querySelector(".robotics");
+
+const dayscholar = document.querySelector(".dayscholar");
+const hosteler = document.querySelector(".hosteler");
+
+function branchandstaying()
+{
+    if(aids.checked)
+    {
+        branch.value = "Airtificial Intelligence and Data Science";
+    }
+}
